25b77be9ee2d54ca84fbb0cee46032df7b717dd7
caickdias <caickandrade0@gmail.com>
2022-10-10 23:42:29 -0300

    changed data get type

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const nedb = require('nedb');
 
 const PORT = process.env.PORT || 8000;
+const DB_PATH = process.env.DB_PATH || 'data.db';
+
+const db = new nedb({ filename: DB_PATH, autoload: true});
 const app = express();
 
 app.use(express.json());
@@ -11,7 +15,19 @@ app.listen(PORT, () => {
 });
 
 // Create
-app.post("/api/students", async (req, res, next) => {
- 
+app.get('/bows', async (req, res, next) => {
+  
+  const fakeData = [{
+    id: 123,
+    type: 'recurve',
+    length: 66,
+    drawWeight: 40,
+    brand: 'samick',
+    modelName: 'polaris',
+    hand: 'right',
+    braceHeight: 7,    
+  }]
+  
+  res.status(200).send(fakeData);
 });
 
