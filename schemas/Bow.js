96f1808761bca1e13ed58dc6ffaf0298e1654250
caickdias <caickandrade0@gmail.com>
2022-10-12 18:52:48 -0300

    added license

--- schemas/Bow.js
+++ schemas/Bow.js
@@ -12,23 +12,23 @@ const postSchema = Joi.object({
     braceHeight: Joi.number(),
   })
   
-  const patchSchema = Joi.object({
-    type: Joi.string(),
-    length: Joi.number(),
-    drawWeight: Joi.number(),
-    brand: Joi.string(),
-    modelName: Joi.string(),
-    hand: Joi.string()
-      .valid("right", "left", "two-handed"),    
-    braceHeight: Joi.number(),
-  })
-  
-  const querySchema = Joi.object({
-    _id: Joi.string().required(),
-  })
+const patchSchema = Joi.object({
+  type: Joi.string(),
+  length: Joi.number(),
+  drawWeight: Joi.number(),
+  brand: Joi.string(),
+  modelName: Joi.string(),
+  hand: Joi.string()
+    .valid("right", "left", "two-handed"),    
+  braceHeight: Joi.number(),
+})
 
-  module.exports = {
-    postSchema,
-    patchSchema,
-    querySchema
+const querySchema = Joi.object({
+  _id: Joi.string().required(),
+})
+
+module.exports = {
+  postSchema,
+  patchSchema,
+  querySchema
   }
